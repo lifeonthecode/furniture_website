5591f48031393022bb1be9cae1e90cae62b02c63
mohammodjuwelislam <mohammodjuwelislam@gmail.com>
2024-06-03 15:31:45 +0600

    furniture website clear

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+
+    // header sticky 
+    $(window).scroll(function(){
+        var sticky = $('.sticky'),
+            scroll = $(window).scrollTop();
+      
+        if (scroll >= 100) sticky.addClass('fixed');
+        else sticky.removeClass('fixed');
+    });
+
+
+    // testimonial slider 
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
